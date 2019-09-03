6f442d698d23708d697fa29e68debf0d9627f15d
FirecoderGithub <firecodergithub@github.com>
2019-09-03 21:32:23 +0300

    fixed the compiling of the images files

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,9 @@ module.exports = {
         ]
       },
       {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        loader: 'url-loader?limit=100000' },
+      {
         test: /\.scss$/,
         use: [{
             loader: "style-loader" // creates style nodes from JS strings
